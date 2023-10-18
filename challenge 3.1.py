cc59adb25dadd351c32305833704f331499e58c0
pachiyammal <148320698+pachiyammal@users.noreply.github.com>
2023-10-18 13:01:23 +0530

    Add files via upload

---
+++ challenge 3.1.py
@@ -0,0 +1,17 @@
+
+#LINEAR SEARCH PRODUCT
+def linearsearchproduct(productlist,targetproduct):
+    indices = []
+    
+    for index,product in enumerate(productlist):
+        if product == targetproduct:
+            indices.append(index)
+            
+            
+    return indices
+
+products: list[str]
+products = ["shoes","boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+result = linearsearchproduct(products,target)
+print(result)
